c02f6713cc199f62226b56f731112dc48d286763
m-pihlak <pihlakmer@gmail.com>
2024-07-24 18:57:36 +0300

    Changed routing
    Added new route, changed route name for landing page route. Switched landing page route alias and path for consistency.
    Added beforeEach, which updates current route index in vuex store before switching to next route

--- src/router/index.js
+++ src/router/index.js
@@ -3,21 +3,27 @@ import VueRouter from 'vue-router'
 
 import AboutMe from '@/views/AboutMe.vue'
 import ResumeView from '@/views/ResumeView.vue'
+import SkillsView from '@/views/SkillsView.vue'
 import ProjectsView from '@/views/ProjectsView.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
     {
-        path: '/',
-        alias: '/aboutme',
-        name: 'aboutme',
+        path: '/aboutme',
+        alias: '/',
+        name: 'about me',
         component: AboutMe
     },
     {
         path: '/resume',
         name: 'resume',
         component: ResumeView
+    },
+    {
+        path: '/skills',
+        name: 'skills',
+        component: SkillsView
     },
     {
         path: '/projects',
@@ -28,6 +34,17 @@ const routes = [
 
 const router = new VueRouter({
   routes
+})
+
+import store from '@/store'
+
+router.beforeEach(async (to, from, next) => {
+    store.dispatch({
+        type: 'updateRouteIndex',
+        path: to.path
+    })
+    from
+    next()
 })
 
 export default router
