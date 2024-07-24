ebcb7b7d0f06fcdba81516e4ac8a22244bb947a4
m-pihlak <pihlakmer@gmail.com>
2024-07-24 18:57:40 +0300

    Create SkillsView.vue

---
+++ src/views/SkillsView.vue
@@ -0,0 +1,28 @@
+<template>
+    <div id="skills">
+        <h1>My Skills</h1>
+        <p>
+            Lorem ipsum dolor sit amet.
+        </p>
+        <p>
+            Lorem ipsum dolor sit.
+        </p>
+        <p>
+            Lorem ipsum dolor sit amet consectetur.
+        </p>
+    </div>
+</template>
+
+<script>
+import '@/assets/css/main.css'
+
+export default {
+    name: 'SkillsView',
+    components: {
+    }
+}
+</script>
+
+<style>
+
+</style>
