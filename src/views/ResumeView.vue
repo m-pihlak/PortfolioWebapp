83621132bec2181b39651f8a1e0f04d8b02fb4f8
m-pihlak <pihlakmer@gmail.com>
2024-07-27 00:45:01 +0300

    h2 styling

--- src/views/ResumeView.vue
+++ src/views/ResumeView.vue
@@ -15,14 +15,9 @@
         
         <h1>My Skills</h1>
         <ContentBlock>
+            <h2>Lorem</h2>
             <p>
-                Lorem ipsum dolor sit amet.
-            </p>
-            <p>
-                Lorem ipsum dolor sit.
-            </p>
-            <p>
-                Lorem ipsum dolor sit amet consectetur.
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nobis sequi accusantium voluptatibus perspiciatis ut, harum quidem? Facere nisi blanditiis adipisci tenetur magni rem sunt magnam exercitationem sequi quis inventore iure eveniet, ipsum eos quaerat maiores est natus quasi reprehenderit dicta delectus? Expedita illo at harum assumenda accusantium voluptas eligendi doloribus.
             </p>
         </ContentBlock>
     </div>
