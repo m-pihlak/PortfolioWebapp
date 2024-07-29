180686171f71e38849910e9bc51c6b3c1a9d02e3
m-pihlak <pihlakmer@gmail.com>
2024-07-29 03:29:51 +0300

    Fixed About Me styling

--- src/views/AboutMe.vue
+++ src/views/AboutMe.vue
@@ -22,10 +22,7 @@ export default {
 .wrapper-about-me {
     .wrapper {
         display: grid;
-        grid-template-areas: 
-                "a a c"
-                "b b c";
-        
+        grid-template-columns: minmax(0, 2fr) minmax(0, 1fr);
         grid-gap: var(--spacing);
     }
 
@@ -38,19 +35,17 @@ export default {
 
     .item-0 {
         grid-row: 1;
-        grid-column: 1/3;
+        grid-column: 1;
     }
 
     .item-1 {
         grid-row: 2;
-        grid-column-start: 1;
-        grid-column-end: -1;
+        grid-column: 1;
     }
 
     .item-2 {
-        grid-column: 3;
-        grid-row-start: 1;
-        grid-row-end: -1;
+        grid-row: 1/3;
+        grid-column: 2;
         
         text-align: center;
         width: 100%;
@@ -58,7 +53,32 @@ export default {
 
     .item-2 > img {
         width: 100%;
-        height: 100px;
+    }
+
+    @media screen and (max-width: 1380px) {
+        .wrapper {
+            grid-template-columns: minmax(0, 1fr) minmax(0, 1fr);
+        }
+
+        .item-1 {
+            grid-row: 2;
+            grid-column: 1/3;
+        }
+
+        .item-2 {
+            grid-row: 1;
+            grid-column: 2;
+            
+            text-align: center;
+            width: 100%;
+        }
+    }
+
+    @media screen and (max-width: 1000px) {
+        .wrapper {
+            display: flex;
+            flex-direction: column;
+        }
     }
 }
 
