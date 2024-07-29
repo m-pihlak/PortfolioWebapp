7d8ffa73af99482dd10f8ee8274ac5e066c212e9
m-pihlak <pihlakmer@gmail.com>
2024-07-30 00:08:51 +0300

    Small width styling fix

--- src/views/AboutMe.vue
+++ src/views/AboutMe.vue
@@ -85,7 +85,7 @@ export default {
 
     @media screen and (max-width: 1000px) {
         .wrapper {
-            display: flex;
+            display: flex !important; 
             flex-direction: column;
         }
     }
